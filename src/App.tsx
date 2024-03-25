c5c80f2b30d87ea5229181ef33bb338b8bdbd25c
Jordson Galvão <105514196+JordsonZ@users.noreply.github.com>
2024-03-25 10:49:11 -0300

    listas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+const tarefas = [
+  { title: 'Limpar casa', id: 1 },
+  { title: 'Lavar louça', id: 2 },
+  { title: 'Dormir', id: 3 },
+];
+const listTarefas = tarefas.map(tarefas =>
+  <li key={tarefas.id}>
+    {tarefas.title}
+  </li>
+);
 const App = () => {
     return (
       <div>
-        <h1>Bem vindo ao mundo React</h1>
+        <h1>Lista de Tarefas</h1>
         <button>eu sou um botão html</button>
+        <ul>{listTarefas}</ul>
       </div>
     );
   }
