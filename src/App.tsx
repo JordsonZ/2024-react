022eead4d2b82055b83789c459f52d2f3c31eb46
Jordson Galvão <105514196+JordsonZ@users.noreply.github.com>
2024-03-26 10:14:59 -0300

    respondendo a eventos e estado

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,75 @@
-const tarefas = [
-  { title: 'Limpar casa', id: 1 },
-  { title: 'Lavar louça', id: 2 },
-  { title: 'Dormir', id: 3 },
-];
-const listTarefas = tarefas.map(tarefas =>
-  <li key={tarefas.id}>
-    {tarefas.title}
-  </li>
-);
+import { useState } from "react";
+
+const ListaDeTarefas = (props: any) => {
+  return (
+    <>
+      <h4>Tarefas como lista de objetos</h4>
+      <ul>
+        {
+          props.dados.map((item: any) => {
+            return <ItemTarefa key={item.id} titulo={item.titulo} />
+          })
+        }
+      </ul>
+    </>
+  );
+}
+
+const ItemTarefa = (props: any) => {
+  return (<li>{props.titulo}</li>);
+}
+
 const App = () => {
-    return (
-      <div>
-        <h1>Lista de Tarefas</h1>
-        <button>eu sou um botão html</button>
-        <ul>{listTarefas}</ul>
-      </div>
-    );
+  const [tarefa, setTarefa] = useState("digite uma tarefa");
+
+  const [tarefas, setTarefas] = useState([
+    {
+      id: 1,
+      titulo: "criar interface react",
+      concluido: false,
+    },
+    {
+      id: 2,
+      titulo: "conectar a uma  api",
+      concluido: false,
+    },
+    {
+      id: 3,
+      titulo: "aprender sobre autenticação",
+      concluido: false,
+    },
+  ]);
+
+  const escutarCliqueBotao = () => {
+    console.log("clicou");
+    console.info(tarefa);
+    const objeto = {
+      id: tarefas.length + 1,
+      titulo: tarefa,
+      concluido: false,
+    }
+    console.info(objeto);
+    // tarefas.push(objeto);
+    setTarefas([
+      ...tarefas,
+      objeto
+    ]);
   }
-  
-  export default App;
-  
+  const escutarModificacaoTexto = (evento: any) => {
+    setTarefa(evento.target.value);
+  }
+
+  return (
+    <div className="aplicacao">
+      <h1>Infoweb - React</h1>
+      <div>
+        <label htmlFor="tarefa">Informe a nova tarefa: </label>
+        <input type="text" id="tarefa" value={tarefa} onChange={escutarModificacaoTexto} />
+        <button onClick={escutarCliqueBotao}>Criar nova tarefa</button>
+      </div>
+      <ListaDeTarefas dados={tarefas}/>
+    </div>
+  );
+}
+
+export default App;
